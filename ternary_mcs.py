f7bb7b1139b897a02e6282a2bfa586f8467af6c6
Jonathan Fuzaro Alencar <jfuzaroa@outlook.com>
2022-04-10 22:35:43 -0700

    Begin using poetry dependency manager and mass/charge sampling

---
+++ ternary_mcs.py
@@ -0,0 +1,9 @@
+import ROOT
+import numpy as np
+import scipy as scp
+import scipy.constants as phys_const
+import matplotlib.pyplot as plt
+from matplotlib import colors as pltc
+from numpy import linalg as la
+from matplotlib import animation as anm
+from scipy.integrate import solve_ivp
